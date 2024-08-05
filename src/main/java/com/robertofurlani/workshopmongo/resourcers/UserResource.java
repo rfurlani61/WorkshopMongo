030137122f57c21ffda7d38dd589b1fed9c5f4a5
rfurlani <furlani.roberto@gmail.com>
2024-08-05 11:15:30 -0300

    Obtendo um usuário por id

--- src/main/java/com/robertofurlani/workshopmongo/resourcers/UserResource.java
+++ src/main/java/com/robertofurlani/workshopmongo/resourcers/UserResource.java
@@ -1,10 +1,10 @@
 package com.robertofurlani.workshopmongo.resourcers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +16,20 @@ import com.robertofurlani.workshopmongo.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
 		return ResponseEntity.ok().body(listDto);
+	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		UserDTO userDto  = new UserDTO(obj);
+		return ResponseEntity.ok().body(userDto);
 	}
 }
